20dacbf4f6258ebf2d33ff2a7761d85496078e0a
风信子 <1181214391@qq.com>
2020-09-02 08:20:02 +0800

    3

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,51 @@
 <template>
-  <div class="home-box">
-    <button @click="loginBtn">退回</button>
-  </div>
+  <el-container class="el-container">
+    <!-- 头部区 -->
+    <el-header class="el-header">
+      <div>
+        <img src="../assets/love.png" alt="">
+        <span>电商管理后台系统</span>
+      </div>
+      <el-button type="info" @click="loginBtn">退出</el-button>
+    </el-header>
+    <!-- 主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside class="el-aside" width="200px">
+        <!-- 侧边栏菜单区域 -->
+         <el-menu background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="1-4-1">选项1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="3">
+              <i class="el-icon-setting"></i>
+              <span slot="title">导航三</span>
+            </el-menu-item>
+          </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main class="el-main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
 export default {
@@ -16,7 +60,34 @@ methods:{
 }
 </script>
 <style lang="less" scoped>
-  .home-box{
+  .el-container{
     height: 100%;
+  }
+  .el-header{
+    background-color: #373d41;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 0;
+
+    >div{
+      display: flex;
+      align-items: center;
+        >img{
+          width: 54px;
+          height: 54px;
+          border-radius: 50%;
+        }
+        >span{
+          margin-left: 20px;
+          color: #ffffff;
+        }
+    }
+  }
+  .el-aside{
+    background-color: #333744;
+  }
+  .el-main{
+    background-color: #eaedf1;
   }
 </style>
