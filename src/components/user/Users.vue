e9fc6ebc138d2a2b2dba46fa5c65c51d7b750a94
风信子 <1181214391@qq.com>
2020-09-03 13:46:41 +0800

    添加用户信息及校验规则

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -9,33 +9,36 @@
     <!-- 卡片视图区域 -->
     <el-card>
       <!-- 搜索与添加区 -->
-
       <el-row :gutter="20">
         <el-col :span="8">
-          <el-input placeholder="请输入内容">
-            <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-input placeholder="请输入内容" v-model='queryInfo.query' clearable @clear='getUserList'>
+            <el-button slot="append" icon="el-icon-search" @click="getUserList"></el-button>
           </el-input>
         </el-col>
         <el-col :span="4">
-          <el-button type="primary">添加按钮</el-button>
+          <el-button type="primary" @click="addDialogVisible = true">添加按钮</el-button>
         </el-col>
       </el-row>
       <!-- 用户列表区域 -->
-       <el-table :data="usersData" border stripe>
+       <el-table :data="userList" border stripe>
+        <el-table-column type="index" label="#"></el-table-column>
         <el-table-column prop="username" label="姓名" ></el-table-column>
         <el-table-column prop="email" label="邮箱" ></el-table-column>
         <el-table-column prop="mobile" label="电话" ></el-table-column>
         <el-table-column prop="role_name" label="角色" ></el-table-column>
         <el-table-column label="状态" >
           <template slot-scope="scope">
-            <el-switch v-model="scope.row.mg_state">
-            </el-switch>
+            <el-switch v-model="scope.row.mg_state" @change="userStateChanged(scope.row)"></el-switch>
           </template>
         </el-table-column>
         <el-table-column label="操作"  width="180px">
-          <template slot-scope="scope">
+          <template >
+            <!-- {{scope.row}}slot-scope="scope" -->
+            <!-- 修改按钮 -->
             <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            <!-- 删除按钮 -->
             <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            <!-- 分配角色按钮 -->
             <el-tooltip content="分配角色" placement="top" :enterable="false">
               <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
             </el-tooltip>
@@ -44,41 +47,105 @@
       </el-table>
       <!-- 分页区域 -->
       <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
-      :current-page="currentPage4" :page-sizes="[1, 2, 5, 10]" :page-size="100"
-      layout="total, sizes, prev, pager, next, jumper" :total="400">
+      :current-page="queryInfo.pagenum" :page-sizes="[1, 2, 5, 10]" :page-size="queryInfo.pagesize"
+      layout="total, sizes, prev, pager, next, jumper" :total="total">
       </el-pagination>
-
     </el-card>
+    <!-- 添加用户对话框 -->
+    <el-dialog title="添加用户" :visible.sync="addDialogVisible"
+      width="50%">
+      <!-- 内容主题区 -->
+      <el-form :model="addFrom" :rules="addFromRules" ref="addFromRef" label-width="70px">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addFrom.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addFrom.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addFrom.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="addFrom.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+      <!-- 底部区域 -->
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 <script>
 export default {
   data(){
     return {
-      usersData: [
-          { email: '2016-05-02',
-            username: '王小虎',
-            mobile: '13336875820',
-            role_name: '大神',
-            mg_state: true,
-          },
-          { email: '2016-05-02',
-            username: '王大虎',
-            mobile: '13336875820',
-            role_name: '大神',
-            mg_state: false,
-          }
-      ]
+      // 获取用户列表
+      queryInfo:{
+        query : '',
+        pagenum : 1,//当前的页数
+        pagesize : 2 //当前每页显示多少条数据
+      },
+      userList:[],
+      total:0,
+      addDialogVisible: false,//控制添加用户对话框的显示与隐藏
+      addFrom: {
+        username:'',
+        password:'',
+        email:'',
+        mobile:''
+      },//添加用户的表单数据
+      addFromRules:{  //添加表单的验证规则对象
+        username:[
+          {required:true,message:'请输入用户名',trigger:'blur'},
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        password:[
+          {required:true,message:'请输入密码',trigger:'blur'},
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ],
+        email:[
+          {required:true,message:'请输入邮箱',trigger:'blur'}
+        ],
+        mobile:[
+          {required:true,message:'请输入手机号',trigger:'blur'}
+        ]
+      }
     }
   },
+  created(){
+    this.getUserList()
+  },
   methods:{
-    // 监听pagesize改变事件
-    handleSizeChange(){
+    async getUserList(){
+      const {data : res} = await this.$http.get('users',{params:this.queryInfo});
+      if(res.meta.status !==200) {
+        return this.message.error('获取用户列表失败！');
+      };
+      this.userList = res.data.users;
+      this.total = res.data.total;
 
     },
+    // 监听pagesize改变事件
+    handleSizeChange(newSize){
+      this.queryInfo.pagesize = newSize;
+      this.getUserList()
+    },
     // 监听页码值改变事件
-    handleCurrentChange(){
-
+    handleCurrentChange(newPage){
+      this.queryInfo.pagenum = newPage;
+      this.getUserList()
+    },
+    // 监听switch开关状态的改变
+    async userStateChanged(userinfo){
+      console.log(userinfo);
+      const {data : res} = await this.$http.put(`users/${userinfo.id}/state/${userinfo.mg_state}`);
+      console.log(res);
+      if(res.meta.status !== 200){
+        return this.$message.error('更新用户状态失败!');
+      }
+      this.$message.success('跟新用户状态成功!')
     }
   }
 }
