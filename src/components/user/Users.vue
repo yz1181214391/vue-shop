931be3680d977c5c54f9ab39b86a6924b9eb1046
风信子 <1181214391@qq.com>
2020-09-03 14:12:39 +0800

    监听用户对话框的关闭事件

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -52,8 +52,7 @@
       </el-pagination>
     </el-card>
     <!-- 添加用户对话框 -->
-    <el-dialog title="添加用户" :visible.sync="addDialogVisible"
-      width="50%">
+    <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="50%" @close="addDialogClosed">
       <!-- 内容主题区 -->
       <el-form :model="addFrom" :rules="addFromRules" ref="addFromRef" label-width="70px">
         <el-form-item label="用户名" prop="username">
@@ -80,6 +79,23 @@
 <script>
 export default {
   data(){
+    //自定义验证邮箱的规则
+    const checkEmail = (rule, value, callback) => {
+      // 邮箱正则
+      const regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/
+      if (regEmail.test(value)) {
+        return callback()
+      }
+      callback(new Error('请输入合法的邮箱'))
+    }
+    const checkMobile = (rule, value, callback) => {
+      // 手机正则
+      const regMobile = /^(0|86|17951)?(13[0-9]|15[0123456789]|17[678]|18[0-9]|14[57])[0-9]{8}$/
+      if (regMobile.test(value)) {
+        return callback()
+      }
+      callback(new Error('请输入合法的手机号'))
+    }
     return {
       // 获取用户列表
       queryInfo:{
@@ -106,14 +122,18 @@ export default {
           { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
         ],
         email:[
-          {required:true,message:'请输入邮箱',trigger:'blur'}
+          {required:true,message:'请输入邮箱',trigger:'blur'},
+          {validator: checkEmail, trigger: 'blur'}
         ],
         mobile:[
-          {required:true,message:'请输入手机号',trigger:'blur'}
+          {required:true,message:'请输入手机号',trigger:'blur'},
+          {validator: checkMobile, trigger: 'blur'}
+
         ]
       }
     }
   },
+  // 生命周期函数
   created(){
     this.getUserList()
   },
@@ -125,7 +145,6 @@ export default {
       };
       this.userList = res.data.users;
       this.total = res.data.total;
-
     },
     // 监听pagesize改变事件
     handleSizeChange(newSize){
@@ -146,6 +165,10 @@ export default {
         return this.$message.error('更新用户状态失败!');
       }
       this.$message.success('跟新用户状态成功!')
+    },
+    //监听用户对话框的关闭事件
+    addDialogClosed(){
+      this.$refs.addFromRef.resetFields()
     }
   }
 }
