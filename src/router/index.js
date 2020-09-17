a28404ee187760184334fcb3d84925fc46082e6a
yz1181214391 <1181214391@qq.com>
2020-09-17 08:44:57 +0800

    3

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
 import Users from '../components/user/Users.vue'
+import Rights from '../components/power/Rights.vue'
 
 Vue.use(VueRouter)
 
@@ -13,9 +14,11 @@ const routes = [
   { path : '/login', component : Login},
   { path : '/home', component : Home,
     redirect : '/welcome',
-    children :[{ path : '/welcome', component : Welcome},
-              { path : '/users', component : Users}
-  ]
+    children :[
+              { path : '/welcome', component : Welcome},
+              { path : '/users', component : Users},
+              { path : '/rights', component : Rights}
+    ]
   }
 ]
 
